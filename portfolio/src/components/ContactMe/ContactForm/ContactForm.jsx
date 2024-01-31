b29f86e08cc18a0725af21e58d79f2927a76820c
amir-38 <a_saruxanov_38@mail.ru>
2024-01-31 19:15:28 +0400

    change access key

--- portfolio/src/components/ContactMe/ContactForm/ContactForm.jsx
+++ portfolio/src/components/ContactMe/ContactForm/ContactForm.jsx
@@ -11,7 +11,7 @@ function ContactForm() {
 
     try {
       const formData = new FormData(event.target);
-      formData.append("access_key", "  1a356a93-543d-4b2d-b504-439a0f895015");
+      formData.append("access_key", "  ACCES KEY");
 
       const object = Object.fromEntries(formData);
       const json = JSON.stringify(object);
